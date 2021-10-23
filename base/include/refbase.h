6661f7f75c53cfd2ae62f84897563f5a70155b88
lengqinjie <15390014138@163.com>
2021-10-23 21:54:28 +0800

    forgotten decrease reference of strong pointer
    Signed-off-by: lengqinjie <15390014138@163.com>

--- base/include/refbase.h
+++ base/include/refbase.h
@@ -336,6 +336,9 @@ template <typename T>
 inline sptr<T> &sptr<T>::operator=(const wptr<T> &other)
 {
     if ((other != nullptr) && other.AttemptIncStrongRef(this)) {
+	    if (refs_ != nullptr) {
+	        refs_->DecStrongRef(this);
+	    }
         refs_ = other.GetRefPtr();
     } else {
         refs_ = nullptr;
